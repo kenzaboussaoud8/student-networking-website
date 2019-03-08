385ff815815f9ee9d9cf706c1f8a264e06469177
vincentteissiercouturier <teissier-couturier.vincent@outlook.fr>
2019-03-08 11:48:06 +0100

    API|changed db connection and SQL requests

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,6 +1,5 @@
-// Imports
-let mysql = require('mysql');
-let config = require('../config');
+let connection = require('../config').mysql;
+let mail = require('../config').smtp;
 
 // Routes
 module.exports = {
@@ -14,26 +13,25 @@ module.exports = {
         var birth_date = req.body.birth_date;
         var student_card = req.body.student_card;
 
-        if (email == null || first_name == null || last_name == null || password == null || birth_date == null || student_card == null){
+        if (email == null || first_name == null || last_name == null || password == null || birth_date == null || student_card == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
 
         // TODO verify pseudo length, mail regex, password etc.
 
-        let connection = mysql.createConnection(config);
-        
-        let sql = `SELECT email FROM Student WHERE email =` + mysql.escape(email);
+        let sql = 'SELECT email FROM User WHERE email = ' + connection.escape(email);
         connection.query(sql, (error, results, fields) => {
-        if (error) {
-            return console.error(error.message);
-        }
-        console.log(results);
+            if (error) {
+                // let sql = `INSERT INTO Student VALUES ('Learn how to insert a new row',true)`;
+            } else {
+                return res.status(400).json({ 'error': 'user already exist' });
+            }
         });
-        
+
         connection.end();
 
     },
-    login: function(req, res){
-        
+    login: function(req, res) {
+
     }
 }
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // Initializing an instance of Express
 var express = require('express');
 var app = express();
@@ -13,13 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Configure routes
-app.get('/',function (req, res){
+app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send('<h1>Hello on my server</h1>');
 });
 
 app.use('/api/', apiRouter);
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log('Server is running on Port', PORT);
 });
