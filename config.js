0e18adbbf3f51f5675a434241cd6a2513c531f02
kenzaboussaoud8 <kboussaoud@outlook.com>
2019-05-03 09:26:14 +0200

    updated gitignore

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret: 'withgreatpowercomesgreatresponsibility',
-    amazon_accessKeyId: 'AKIAJQNQRDOAS3223YOA',
-    amazon_secretAccessKey: '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'
-  };
