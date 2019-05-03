5346ed84d8ac85ca9428fb9fd2468ba0aa59e7a9
kenzaboussaoud8 <kboussaoud@outlook.com>
2019-05-03 09:23:08 +0200

    added amazon security ids in config file (gitignored)

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    secret: 'withgreatpowercomesgreatresponsibility'
+    secret: 'withgreatpowercomesgreatresponsibility',
+    amazon_accessKeyId: 'AKIAJQNQRDOAS3223YOA',
+    amazon_secretAccessKey: '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'
   };
